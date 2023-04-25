cb606f7e68eafe17835c38537b58188cb5c72ba8
Rafael Ronsoni Gaidzinski <rafael.513011@alunosatc.edu.br>
2023-04-24 21:19:07 -0300

    Exercicio 2 da lista de exercicio da aula 8 atualizado

--- Aula 8 - Lista de Exercícios/Ex2.py
+++ Aula 8 - Lista de Exercícios/Ex2.py
@@ -1,20 +1,32 @@
 #   2) Elabore um programa que leia um número inteiro e indique se o número é primo
 #   ou não. Lembrando que os números primos são divisíveis somente por 1 e por ele
 #   mesmo. No entanto, o número 1 não é primo.
+   
+import math, time
 
 print("Programa para verificar se um número é primo")
 
 numero = int(input("Informe um número para verificar se é primo: "))
 
-divisor = 1
 contador = 0
+divisor = 2
 
-while (divisor <= numero):
+start = time.time()
+
+raiz_numero = int(math.sqrt(numero) + 1)   
+
+while (contador < 1 and divisor <= raiz_numero):
+    if (numero <= 3):
+        break
     if (numero % divisor == 0):
         contador += 1
     divisor += 1
 
-if (contador <= 2):
-    print(f"O número {numero} é primo!")
-else:
+end = time.time()
+
+if (contador > 0 or numero == 1):
     print(f"O número {numero} não é primo!")
+    print(f"Tempo de execução: {end - start: .2f} seg")
+else:
+    print(f"O número {numero} é primo!") 
+    print(f"Tempo de execução: {end - start: .2f} seg")
